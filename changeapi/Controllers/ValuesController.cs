4a92e2360886c472e8fae0e2ffa3936c7e60d6a9
Sakul <miolynet@perfenterprise.com>
2018-11-03 16:25:04 +0700

    Complete web api.

---
+++ changeapi/Controllers/ValuesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using changelib;
+using Microsoft.AspNetCore.Mvc;
+
+namespace changeapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values/5/9
+        [HttpGet("{price}/{amount}")]
+        public ActionResult<ChangeResult> Get(int price, int amount)
+        {
+            var change = new ChangeCalculator();
+            return change.CalChange(price, amount);
+        }
+    }
+}
