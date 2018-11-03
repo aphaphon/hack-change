28f40532d54b3619b308d5dc9e1638938cc18af9
Sakul <miolynet@perfenterprise.com>
2018-11-03 17:02:02 +0700

    Validate user inputs.

--- changemobile/src/app/home/home.page.ts
+++ changemobile/src/app/home/home.page.ts
@@ -18,22 +18,29 @@ export class HomePage {
   private twenty: number = 0;
   private five: number = 0;
   private one: number = 0;
+  private mss: string = "";
 
   constructor(private http: HttpClient) {
   }
 
   private Submit() {
+
+    if (this.productPrice <= 0 || this.inputMoney <= 0 || (this.productPrice > this.inputMoney)) {
+      this.mss = "ใส่ข้อมูลไม่เหมาะสม";
+      return;
+    }
+
     var url = 'http://localhost:5000/api/values/' + this.productPrice + '/' + this.inputMoney;
     this.http.get<any>(url)
       .subscribe(it => {
         this.respChangeAmount = it.changeAmount;
-         this.thousand = it.changeBanks[0];
-         this.fiveHundred = it.changeBanks[1];
-         this.oneHundred = it.changeBanks[2];
-         this.fifty = it.changeBanks[3];
-         this.twenty = it.changeBanks[4];
-         this.five = it.changeBanks[5];
-         this.one = it.changeBanks[6];
+        this.thousand = it.changeBanks[0];
+        this.fiveHundred = it.changeBanks[1];
+        this.oneHundred = it.changeBanks[2];
+        this.fifty = it.changeBanks[3];
+        this.twenty = it.changeBanks[4];
+        this.five = it.changeBanks[5];
+        this.one = it.changeBanks[6];
       });
   }
 
